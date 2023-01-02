6bc082050ff062e8d0c3ebb83ce2774055f45e08
sidharth512 <papulu.pattanaik@gmail.com>
2023-01-02 21:57:01 +0530

    Update HOOKs for Stepdefination file

--- src/test/java/testRunner/TestRunner.java
+++ src/test/java/testRunner/TestRunner.java
@@ -5,7 +5,7 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = ".//Features",
+@CucumberOptions(features = ".//Features/Login.feature",
 glue = "StepDefination", dryRun = false,
 monochrome = true,
 plugin = {"pretty","html:target/HtmlReports/test-output.html","json:target/Json/Report.json","junit:target/Junit/Report.xml"},
